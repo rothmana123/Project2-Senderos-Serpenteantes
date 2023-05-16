b43bbf6629231ca2ad266064ab722a9e7f425dbb
rothmana123 <asrothman1@usfca.edu>
2023-05-16 11:01:11 -0700

    edit/delete are working

--- controllers/trails.js
+++ controllers/trails.js
@@ -5,6 +5,9 @@ module.exports = {
     new: newTrailPage,
     create: createTrail,
     delete: deleteTrail,
+    show,
+    edit: editTrailPage,
+    update,
   };
 
   function index(req, res) {
@@ -56,20 +59,68 @@ module.exports = {
           });
     }
 
-  function deleteTrail(req, res) {
-    console.log("****This is Delete Function debugging****")
+function deleteTrail(req, res) {
+  console.log("****This is Delete Function debugging****")
 
+  if (!req.isAuthenticated()) {
+    console.log("the user is not authenticated")
+    return res.redirect('/trails'); // Redirect to the login page or handle unauthorized access
+  }
     if (!req.isAuthenticated()) {
-      console.log("the user is not authenticated")
-      return res.redirect('/trails'); // Redirect to the login page or handle unauthorized access
-    }
-      if (!req.isAuthenticated()) {
-  return res.redirect('/login');
+return res.redirect('/login');
 }
 
   Trails.deleteOne({ _id: req.params.id, user: req.user._id })
     .then(() => {
       res.redirect('/trails');
+    })
+    .catch((error) => {
+      console.error(error);
+      res.status(500).send('Internal Server Error');
+    });
+}
+
+function show(req, res) {
+  console.log("******show route was hit");
+  Trails.findById(req.params.id)
+    .then((trail) => {
+      console.log(trail);
+      res.render('trails/show', { trail });
+    })
+    .catch((error) => {
+      console.log(error);
+      res.status(500).send('Error finding trail');
+    });
+}
+
+function editTrailPage(req, res) {
+  console.log("***Edit route was hit")
+  Trails.findById(req.params.id)
+    .then((trail) => {
+      console.log(trail);
+      res.render('trails/edit', { trail });
+    })
+    .catch((error) => {
+      console.log(error);
+      res.status(500).send('Error finding trail');
+    });
+}
+
+function update(req, res) {
+  console.log("***update function hit****")
+  const trailId = req.params.id;
+
+  // Find the trail by ID and update its fields
+  Trails.findByIdAndUpdate(trailId, req.body, { new: true })
+    .then((updatedTrail) => {
+      if (!updatedTrail) {
+        // Trail not found
+        console.log('Trail not found')
+        return res.redirect('/trails');
+      }
+
+      // Redirect to the updated trail's show page or any other desired route
+      res.redirect(`/trails/${updatedTrail._id}`);
     })
     .catch((error) => {
       console.error(error);
--- routes/trails.js
+++ routes/trails.js
@@ -10,8 +10,18 @@ router.get('/', trailControl.index);
 //Get /trails/new
 router.get('/new', trailControl.new)
 
+//Get /trails/:id
+router.get('/:id', trailControl.show)
+
 //POST /trails
 router.post('/', trailControl.create);
+
+//GET /trails/:id/edit
+router.get('/:id/edit', trailControl.edit)
+
+//PUT //trails/:id 
+router.put('/:id', trailControl.update)
+//GET
 
 //Delete /trails
 router.delete('/:id', trailControl.delete);
